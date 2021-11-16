51ebf260f205db210450e18e6c0b9d58160e09f3
tamiozzo <tamiozzo98@icloud.com>
2021-11-16 18:57:22 -0300

    index_controller retornando a lista de produtos

--- application/controller/index_controller.py
+++ application/controller/index_controller.py
@@ -1,9 +1,13 @@
+from application.model.entily.product import Product
+from application.model.dao.productdao import ProductDao
 from flask import render_template
 from application import app
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 @app.route('/list')
 def listarProdutos():
-    return render_template("product_list.html")
+    lista_produtos = ProductDao().listar_produtos()
+    return render_template("product_list.html",lista_produtos = lista_produtos)
--- application/model/dao/productdao.py
+++ application/model/dao/productdao.py
@@ -1,10 +1,28 @@
 from application.model.entily.product import  Product
+import json
 class ProductDao():
-    def __init__(self):
-        self.__products_list = []
-
-    def adicionar_produto(self,produto: Product):
-        self.__products_list.append(produto)
 
     def listar_produtos(self):
-        return self.__products_list
+        result = []
+        with open('products.json', 'r') as file:
+            product_list = json.load(file)
+            result = self.converter_dict_to_list(product_list)
+        return result
+
+
+    def converter_dict_to_list(self,produtos):
+        resultado=[]
+
+        for produto in produtos:
+            product = Product()
+            product.setId(produto['id'])
+            product.setName(produto['name'])
+            product.setImage(produto['image'])
+            product.setOldPrice(produto['oldPrice'])
+            product.setPrice(produto['price'])
+            product.setDescription(produto['description'])
+            product.setCount(produto['installments']['count'])
+            product.setValue(produto['installments']['value'])
+            resultado.append(product)
+
+        return resultado
