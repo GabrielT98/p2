168a64a36dc6c5fa4b7a45b313960e9c740425ca
tamiozzo <tamiozzo98@icloud.com>
2021-11-16 17:54:26 -0300

    adicionado ajax para carregar a lista de produtos

--- application/controller/index_controller.py
+++ application/controller/index_controller.py
@@ -4,3 +4,6 @@ from application import app
 @app.route('/')
 def index():
     return render_template("index.html")
+@app.route('/list')
+def listarProdutos():
+    return render_template("product_list.html")
